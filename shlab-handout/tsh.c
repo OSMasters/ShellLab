5924c513000ee5464316f36cc1af329303777a7e
snaebjorn13 <snaebjorn13@ru.is>
2015-02-10 23:28:11 +0000

    removed trailing ':' from command not found error message

--- shlab-handout/tsh.c
+++ shlab-handout/tsh.c
@@ -286,7 +286,7 @@ void eval(char *cmdline)
 
 	    if(execve(argv[0], argv, environ) < 0) {
                 /* Incorrect command, abort */
-		printf("%s: Command not found:\n", argv[0]);
+		printf("%s: Command not found\n", argv[0]);
 		exit(0);
 	    }
 	}
