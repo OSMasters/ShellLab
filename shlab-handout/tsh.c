a223cb470b53e47e2447e83770e591d4b04e9cd7
snaebjorn13 <snaebjorn13@ru.is>
2015-02-12 19:28:02 +0000

    iz complete

--- shlab-handout/tsh.c
+++ shlab-handout/tsh.c
@@ -188,8 +188,7 @@ pid_t Waitpid(pid_t pid, int *iptr, int options)
 {
     pid_t retpid;
 
-    if((retpid = waitpid(pid, iptr, options)) < 0)
-        unix_error("Waitpid error");
+    retpid = waitpid(pid, iptr, options);
     return retpid;
 }
 
